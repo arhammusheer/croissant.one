a8e12f46df5c7a742fd3b1f7e4e87b877f608a2b
arhammusheer <arhammusheer@gmail.com>
2022-11-17 03:06:47 -0500

    Added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import theme from "./theme";
+import ReactGA from "react-ga";
+import { ANALYTICS_MEASUREMENT_ID } from "./pages/analytics";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -12,3 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </ChakraProvider>
   </React.StrictMode>
 );
+
+ReactGA.initialize(ANALYTICS_MEASUREMENT_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
