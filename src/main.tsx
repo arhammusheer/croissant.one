6109d85d9edcee5bc3c755e49c12e6b7d8120e2a
arhammusheer <arhammusheer@gmail.com>
2024-07-18 18:48:51 +0000

    Added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider, LightMode } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <App />
+      <Analytics />
     </ChakraProvider>
   </React.StrictMode>
 );
