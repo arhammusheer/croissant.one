a7e851f3a5bfbf78a9177dd04b3aca6d0c06e5ca
arhammusheer <arhammusheer@gmail.com>
2024-09-10 00:47:23 -0400

    Refactor Footer component: Update layout and styling

--- src/pages/common/Footer.tsx
+++ src/pages/common/Footer.tsx
@@ -1,4 +1,12 @@
-import { Container, Stack, Text, useColorModeValue } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Image,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { me } from "../../me";
 
 export const Footer = () => {
   return (
@@ -9,15 +17,36 @@ export const Footer = () => {
         align={"center"}
         justify={"center"}
         direction={"column"}
-        borderTop={"1px solid"}
-        borderTopColor={useColorModeValue("brand.200", "brand.700")}
       >
+        <BrandedDivider />
         <Text fontWeight={"bold"}>Made with ❤️ by arhammusheer</Text>
         <Text>
           All rights reserved © Mohammad Arham {new Date().getFullYear()}
         </Text>
       </Stack>
     </Container>
+  );
+};
+
+const BrandedDivider = () => {
+  const borderColor = useColorModeValue("brand.200", "brand.700");
+  return (
+    <Stack
+      direction={"row"}
+      w={"full"}
+      align={"center"}
+      justify={"center"}
+      spacing={4}
+      py={4}
+    >
+      <Box w={"full"} h={"1px"} bg={borderColor} />
+      <Image
+        src={"/assets/croissant.svg"}
+        alt={`${me.name} profile picture`}
+        boxSize={8}
+      />
+      <Box w={"full"} h={"1px"} bg={borderColor} />
+    </Stack>
   );
 };
 
