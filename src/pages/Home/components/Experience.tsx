3ce98483157c2532586efe3f7ffc69a6ff33ee0b
arhammusheer <arhammusheer@gmail.com>
2024-10-15 12:57:26 -0400

    Experience is collapsible

--- src/pages/Home/components/Experience.tsx
+++ src/pages/Home/components/Experience.tsx
@@ -1,16 +1,20 @@
 import {
   Box,
+  Collapse,
   Flex,
   Heading,
+  Icon,
   Stack,
   Text,
   useColorModeValue,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { motion, useAnimation } from "framer-motion";
 import { useEffect } from "react";
 import ReactGA from "react-ga4";
 import { useInView } from "react-intersection-observer";
 import { me } from "../../../me";
+import { FaChevronRight } from "react-icons/fa";
 
 export const Experience = () => {
   return (
@@ -24,7 +28,7 @@ export const Experience = () => {
       p={8}
       tabIndex={-1}
     >
-      <Box>
+      <Box w={"full"} maxW={"container.xl"}>
         <Heading as={"h2"} fontSize={"3xl"} px={{ base: 0, md: 8 }}>
           Experience
         </Heading>
@@ -59,6 +63,9 @@ const SingleExperience = ({
 }) => {
   const [ref, inView] = useInView({ triggerOnce: true });
   const controls = useAnimation();
+  const { isOpen, onToggle } = useDisclosure();
+
+  const borderColor = useColorModeValue("brand.200", "brand.900");
 
   const variants = {
     hidden: { opacity: 0, y: 20, transition: { duration: 1 } },
@@ -92,6 +99,8 @@ const SingleExperience = ({
       borderRadius={"xl"}
       spacing={4}
       tabIndex={0}
+      onClick={onToggle}
+      cursor={"pointer"}
     >
       <Stack spacing={0}>
         <Text
@@ -101,24 +110,54 @@ const SingleExperience = ({
         >
           {duration}
         </Text>
-        <Heading as={"h3"} fontSize={{ base: "3xl", md: "4xl" }}>
-          {company}
-        </Heading>
+        <Flex align={"center"} ml={{ base: -4, md: -8 }}>
+          <Flex
+            as={motion.div}
+            animate={{ rotate: isOpen ? 90 : 0 }}
+            p={{ base: 1, md: 2 }}
+          >
+            <Icon
+              as={FaChevronRight}
+              boxSize={{
+                base: 2,
+                md: 4,
+              }}
+              color={"brand.500"}
+            />
+          </Flex>
+          <Heading as={"h3"} fontSize={{ base: "3xl", md: "4xl" }}>
+            {company}
+          </Heading>
+        </Flex>
         <Text as={"h4"} fontSize={{ base: "2xl", md: "3xl" }}>
           {position}
         </Text>
       </Stack>
-      <Heading as={"span"} fontSize={"xl"} fontWeight={"normal"} mr={2}>
-        {location}
-      </Heading>
-
-      <Stack direction={"column"} spacing={2}>
-        {bullets.map((bullet) => (
-          <Heading as={"h5"} fontSize={"lg"} fontWeight={"normal"} key={bullet}>
-            - {bullet}
+      <Collapse in={isOpen} animateOpacity unmountOnExit>
+        <Stack
+          spacing={4}
+          borderRadius={"xl"}
+          p={4}
+          borderColor={borderColor}
+          borderWidth={1}
+        >
+          <Heading as={"span"} fontSize={"xl"} fontWeight={"normal"} mr={2}>
+            {location}
           </Heading>
-        ))}
-      </Stack>
+          <Stack direction={"column"} spacing={2}>
+            {bullets.map((bullet) => (
+              <Heading
+                as={"h5"}
+                fontSize={"lg"}
+                fontWeight={"normal"}
+                key={bullet}
+              >
+                - {bullet}
+              </Heading>
+            ))}
+          </Stack>
+        </Stack>
+      </Collapse>
     </Stack>
   );
 };
