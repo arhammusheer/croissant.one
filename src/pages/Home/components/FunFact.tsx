700d9d59d363dbe9535a34e37030d0db73bd682d
arhammusheer <arhammusheer@gmail.com>
2024-10-15 20:42:51 -0400

    Refactor FunFact component: Add functionality to wait for 5 seconds before transitioning to the next fact

--- src/pages/Home/components/FunFact.tsx
+++ src/pages/Home/components/FunFact.tsx
@@ -5,19 +5,31 @@ import { me } from "../../../me";
 
 const FunFact: React.FC = () => {
   const [currentFact, setCurrentFact] = useState(0);
+  const [wait, setWait] = useState(5000);
   const [randomFacts] = useState(
     me.facts?.sort(() => Math.random() - 0.5) || []
   );
 
   const funFactColor = useColorModeValue("brand.800", "brand.100");
 
+  const next = () => {
+    setCurrentFact((prev) => (prev + 1) % randomFacts.length);
+  };
+
+  const onClick = () => {
+    next();
+    setWait(5000);
+  };
+
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentFact((prev) => (prev + 1) % randomFacts.length);
-    }, 5000);
+      next();
+    }, wait);
 
-    return () => clearInterval(interval);
-  }, []);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [currentFact, wait]);
 
   if (!randomFacts.length) {
     return <></>;
@@ -35,19 +47,19 @@ const FunFact: React.FC = () => {
       justify={"center"}
       px={4}
     >
-        <Text
-          maxW={"fit-content"}
-          fontSize={"lg"}
-          color={useColorModeValue("brand.600", "brand.300")}
-          fontWeight={"bold"}
-          bg={useColorModeValue("brand.100", "brand.900")}
-          borderRadius={"xl"}
-          px={2}
-          border={"1px"}
-          borderColor={useColorModeValue("brand.200", "brand.600")}
-        >
-          Fun Fact
-        </Text>
+      <Text
+        maxW={"fit-content"}
+        fontSize={"lg"}
+        color={useColorModeValue("brand.600", "brand.300")}
+        fontWeight={"bold"}
+        bg={useColorModeValue("brand.100", "brand.900")}
+        borderRadius={"xl"}
+        px={2}
+        border={"1px"}
+        borderColor={useColorModeValue("brand.200", "brand.600")}
+      >
+        Fun Fact
+      </Text>
       <AnimatePresence mode={"wait"}>
         <motion.div
           key={currentFact}
@@ -56,7 +68,7 @@ const FunFact: React.FC = () => {
           exit={{ opacity: 0, y: "-50%" }} // Exit animation
           transition={{ duration: 0.5 }} // Animation duration
         >
-          <Text fontSize={"lg"} color={funFactColor}>
+          <Text fontSize={"lg"} color={funFactColor} onClick={onClick}>
             {randomFacts[currentFact]}
           </Text>
         </motion.div>
